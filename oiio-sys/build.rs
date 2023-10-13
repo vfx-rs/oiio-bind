daaa589d473b8639f757216b599264b26fc3a923
David Aguilar <davvid@gmail.com>
2023-10-13 09:43:16 -0700

    oiio-sys/build.rs: generalize how OpenImageIO is found
    Use pkg-config to find OpenImageIO so that we can finish off
    the TODO in build.rs about the include paths.
    
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- oiio-sys/build.rs
+++ oiio-sys/build.rs
@@ -10,12 +10,12 @@ fn main() -> Result<()> {
         return Ok(());
     }
 
+    let pkgconfig = pkg_config::probe_library("OpenImageIO")?;
+
     cxx_build::bridges(NAMES.iter().map(|s| format!("src/{}.rs", s)))
         .files(NAMES.iter().map(|s| format!("src/ffi_{}.cpp", s)))
         .flag_if_supported("-std=c++17")
-        // TODO: This is a hack to get it to compile on my machine. Should
-        // probably use CMake or something else.
-        .includes(&["/home/scott/Projects/oiio/dist/include", "/usr/include"])
+        .includes(&pkgconfig.include_paths)
         .compile("oiio-sys");
 
     println!("cargo:rerun-if-changed=src/lib.rs");
