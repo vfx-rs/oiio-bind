1915769838f46162393a952ea1eebb3c9f115956
David Aguilar <davvid@gmail.com>
2023-10-13 09:42:27 -0700

    oiio-sys/build.rs: skip linking on docs.rs
    This makes it possible to generate docs on docs.rs by skipping the link step.
    The docsrs config is enabled so that code can detect when docs.rs is building.
    Enable only a single target to speed up docs.rs builds.
    
    Signed-off-by: David Aguilar <davvid@gmail.com>

--- oiio-sys/build.rs
+++ oiio-sys/build.rs
@@ -3,6 +3,13 @@ use anyhow::Result;
 const NAMES: &[&str] = &["typedesc", "imageio", "deepdata", "filesystem"];
 
 fn main() -> Result<()> {
+    // Skip linking on docs.rs: https://docs.rs/about/builds#detecting-docsrs
+    let building_docs = std::env::var("DOCS_RS").is_ok();
+    if building_docs {
+        println!("cargo:rustc-cfg=docsrs");
+        return Ok(());
+    }
+
     cxx_build::bridges(NAMES.iter().map(|s| format!("src/{}.rs", s)))
         .files(NAMES.iter().map(|s| format!("src/ffi_{}.cpp", s)))
         .flag_if_supported("-std=c++17")
